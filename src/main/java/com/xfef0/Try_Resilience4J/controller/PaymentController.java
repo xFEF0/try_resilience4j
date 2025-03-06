ec9d5051c4bddda7830c2efbbe3f539fe4687632
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2025-03-06 15:55:11 -0300

    add Retry mechanism. Change resilience4j mechanism by changing the order execution of CircuitBreaker and Retry

--- src/main/java/com/xfef0/Try_Resilience4J/controller/PaymentController.java
+++ src/main/java/com/xfef0/Try_Resilience4J/controller/PaymentController.java
@@ -2,21 +2,31 @@ package com.xfef0.Try_Resilience4J.controller;
 
 import com.xfef0.Try_Resilience4J.service.IPaymentService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalTime;
+
 @RequiredArgsConstructor
 @RestController
 public class PaymentController {
 
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class);
+    private static final String PROCESS_PAYMENT = "processPayment";
+
     private final IPaymentService paymentService;
 
     @PostMapping("/processPayment")
-    @CircuitBreaker(name = "processPayment", fallbackMethod = "fallbackProcessPayment")
+    @CircuitBreaker(name = PROCESS_PAYMENT, fallbackMethod = "fallbackProcessPayment")
+    @Retry(name = PROCESS_PAYMENT)
     public ResponseEntity<String> processPayment() {
+        log.info("ProcessPayment invoked at {}", LocalTime.now());
         String result = paymentService.processPayment();
         return ResponseEntity.ok(result);
     }
